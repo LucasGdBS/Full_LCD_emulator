0b76871b7155ba3a8b8baf130786905f4df251f0
Lucas-PC <lgbs@cesar.school>
2024-03-21 09:04:15 -0300

    adding buttons

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const pix = document.querySelectorAll(".dot-px");
 
 pix.forEach((pix) => {
   pix.addEventListener("click", function () {
-    this.classList.toggle('high');
+    this.classList.toggle("high");
+  });
+});
+
+const turn_all_on = document.querySelector("#turn_all_on");
+const turn_all_off = document.querySelector("#turn_all_off");
+const toggle_all = document.querySelector("#toggle_all");
+
+turn_all_on.addEventListener("click", function () {
+  pix.forEach((pix) => {
+    pix.classList.add('high')
+  });
+});
+
+turn_all_off.addEventListener("click", function () {
+  pix.forEach((pix) => {
+    pix.classList.remove('high')
+  });
+});
+
+toggle_all.addEventListener("click", function () {
+  pix.forEach((pix) => {
+    pix.classList.toggle('high')
   });
 });

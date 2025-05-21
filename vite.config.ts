740fda1beaf2f473ab071adc8aa7915fbc23b14c
LucasGdbs <lgbs@cesar.school>
2025-05-21 00:26:49 -0300

    build: preparing to deploy on gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/Full_LCD_emulator",
+});
